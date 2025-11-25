c1039623b4d6d213ef747fc695453b553eddff30
drewpl2021 <linoplas0@gmail.com>
2025-11-25 12:58:15 -0500

    cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   //apiUrl: 'https://tu-dominio.com/api' // prod
-  apiUrl: 'http://localhost:8080/api', // prod
-  authUrl: 'http://localhost:8080/api/auth',   // base para auth
+  apiUrl: 'https://noticia-bakc.onrender.com/api', // prod
+  authUrl: 'https://noticia-bakc.onrender.com/api/auth',   // base para auth
   culqiPublicKey: 'pk_test_o1pxG1u67GWlmQSa', // tu pk_test real
 
 };
